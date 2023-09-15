b145a893796ba155b6e255696a9ae88a8763e403
Tanvir Alam Mugdha <115610092+tanviralammugdha@users.noreply.github.com>
2023-09-15 19:56:37 +0600

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author MY PC
+ */
+import java.util.Scanner;
+public class JavaApplication2 {
+
+
+    public static void main(String[] args) {
+  Scanner sc= new Scanner (System.in);
+  System.out.print("Enter the number01 :");
+  int a =sc.nextInt();
+  System.out.print("enter the number 02 :");
+  int b =sc.nextInt();
+  int add=a+b;
+  System.out.println("the ans is "+add);
+          
+    }
+    
+}
