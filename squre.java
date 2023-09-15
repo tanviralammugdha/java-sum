3a7ccceba8efae67207148ff46c130bd9ec5f0b5
Tanvir Alam Mugdha <115610092+tanviralammugdha@users.noreply.github.com>
2023-09-15 21:04:22 +0600

    Add files via upload

---
+++ squre.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author MY PC
+ */
+ import java.util.Scanner;
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+  Scanner sc= new Scanner (System.in);
+  System.out.print("Enter the lenth :");
+  double a =sc.nextInt();
+  double area=a*a;
+  System.out.println("the squre area is "+area);
+    }
+    
+}
