ab31d9e64da43c6affc2f7bbd486bcf8abc2b777
Tanvir Alam Mugdha <115610092+tanviralammugdha@users.noreply.github.com>
2023-09-15 20:50:59 +0600

    Add files via upload

---
+++ avarage.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author MY PC
+ */
+import java.util.Scanner;
+public class JavaApplication2 {
+
+
+    public static void main(String[] args) {
+ Scanner sc= new Scanner(System.in);
+ System.out.print("enter the number of :");
+ int a= sc.nextInt();
+ System.out.print("enter the number of :");
+ int b= sc.nextInt();
+ System.out.print("enter the number of :");
+ int c= sc.nextInt();
+ int avarage = (a+b+c)/3;
+ System.out.print("avarage area is"+avarage);
+
+ 
+ 
+    }
+    
+}
