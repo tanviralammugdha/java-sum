d80bad1f02a0a04db4f62bc3597be7489e36e8aa
Tanvir Alam Mugdha <115610092+tanviralammugdha@users.noreply.github.com>
2023-09-16 10:39:02 +0600

    Add files via upload

---
+++ smallnumber.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author MY PC
+ */
+import java.util.Scanner;
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the first number  :");
+        int a= sc.nextInt();
+        System.out.print("enter the second number  :");
+        int b= sc.nextInt();
+        if(a<b){
+        System.out.print("small number is :"+a);
+        }
+        else{
+        System.out.print(" small number is:"+b);
+        }
+        
+        
+    }
+    
+}
